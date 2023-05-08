174c94dcc9c4f47caa453fd1cdb5516df5cde4a6
Shibendra Bhattacharjee <storebshiv@gmail.com>
2023-05-08 20:52:20 +0530

    Model Evaluation_testing_v1

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -55,3 +55,12 @@ class ModelTrainerConfig:
             MODEL_FILE_NAME
         )
         self.expected_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
+
+
+class ModelEvaluationConfig: 
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_evaluation_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, MODEL_EVALUATION_DIR_NAME
+        )
+        self.report_file_path = os.path.join(self.model_evaluation_dir,MODEL_EVALUATION_REPORT_NAME)
+        self.change_threshold = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
